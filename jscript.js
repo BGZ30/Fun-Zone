a7dbc5cd4030777f1acbbd53bb68baf4df01ddb7
Bassant Gamal <44124521+BGZ30@users.noreply.github.com>
2019-09-29 23:30:50 +0200

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,7 @@
+function showMainMenu(className){
+	// Get a NodeList of all .demo elements
+  var menus = document.querySelectorAll(className);
+	// Change the text of multiple elements with a loop
+	menus.forEach(element => {element.style.display = "block";})
+
+}
