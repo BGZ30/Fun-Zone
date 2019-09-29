4f6f4e88917deb9aa511183b9c29f5fd5ae07819
Bassant Gamal <44124521+BGZ30@users.noreply.github.com>
2019-09-30 01:29:36 +0200

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,5 +1,7 @@
-function showMainMenu(className,disp){
+function showMainMenu(btn,className,disp){
 	// Get a NodeList of all .demo elements
+  var btnCol=document.getElementById(btn);
+  btnCol.style.opacity=0.5;
   var menus = document.querySelectorAll(className);
 	// Change the text of multiple elements with a loop
 	menus.forEach(element => {element.style.display = disp;})
